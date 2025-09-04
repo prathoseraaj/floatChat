881e4a2fe70902dab189a9fdb861e5e4f1fe8198
prathoseraaj <prathoseraaj0312@gmail.com>
2025-09-04 20:54:21 +0530

    feat: query to extract the database as csv, just for reading

--- database/postgres.sql
+++ database/postgres.sql
@@ -3,3 +3,6 @@ SELECT * FROM argo_profiles;
 
 --QUERY TO REMOVE THE NULL VALUES FROM THE TABLE
  DELETE FROM argo_profiles WHERE pressure is NULL AND temperature is NULL AND salinity is NULL;
+
+ --CPOY THE POSTGRES TABLE INTO CSV
+ COPY argo_profiles TO '/tmp/argo_profiles.csv' DELIMITER ',' CSV HEADER;
