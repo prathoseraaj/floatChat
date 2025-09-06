8a614988b62c48c44e38af9ba3583cb3fda76e91
prathoseraaj <prathoseraaj0312@gmail.com>
2025-09-06 16:51:04 +0530

    feat: collection and discription for our knowledge

--- backend/vector_db.py
+++ backend/vector_db.py
@@ -14,3 +14,19 @@ google_ef = embedding_functions.GoogleGenerativeAiEmbeddingFunction(api_key=gemi
 
 client = chromadb.PersistentClient(path='chroma_db')
 
+collection = client.get_or_create_collection(name='argo_schema', embedding_function=google_ef)
+
+#discription this is for our knowledge
+schema_description = """
+The user can query a PostgreSQL database table named 'argo_profiles'.
+This table contains oceanographic data from ARGO floats and has the following columns:
+- platform_id: The unique identifier for the ARGO float.
+- cycle_number: The specific profile cycle number for the float.
+- latitude: The latitude of the measurement in degrees.
+- longitude: The longitude of the measurement in degrees.
+- timestamp: The date and time of the measurement.
+- pressure: The pressure in decibars, which indicates the depth.
+- temperature: The water temperature in Celsius.
+- salinity: The water salinity in Practical Salinity Units.
+The table is clean and contains no null values.
+"""
